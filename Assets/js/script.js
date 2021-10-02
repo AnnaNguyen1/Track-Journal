fa4073c75d4a95e5af526ea5110a3f7c03332c6f
Anna <an9062@gmail.com>
2021-10-02 11:37:40 +1000

    css styling changes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
-var quote = document.querySelector('#quote');
-var quoteAuthor = document.querySelector('#author');
+var quote = document.querySelector("#quote");
+var quoteAuthor = document.querySelector("#author");
 var autocompleteDiv = document.querySelector("#userLocation");
 var submitAddressSearch = document.querySelector("#searchBtn");
 var radiusInputVal = document.querySelector("#userRadius");
@@ -7,23 +7,22 @@ var radiusInputVal = document.querySelector("#userRadius");
 var lat = "";
 var lng = "";
 
-
 function getQuote() {
-    fetch("https://type.fit/api/quotes")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-    var randomI = Math.floor(Math.random() * data.length);
-    console.log(randomI);
-    var displayQuote = data[randomI].text;
-    var displayAuthor = data[randomI].author;
-    quoteAuthor.textContent = displayAuthor;
-    quote.textContent = displayQuote;
-  });
-} 
-getQuote()
+  fetch("https://type.fit/api/quotes")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var randomI = Math.floor(Math.random() * data.length);
+      console.log(randomI);
+      var displayQuote = data[randomI].text;
+      var displayAuthor = data[randomI].author;
+      quoteAuthor.textContent = displayAuthor;
+      quote.textContent = displayQuote;
+    });
+}
+getQuote();
 
 function initAutocomplete() {
   let autocomplete = new google.maps.places.Autocomplete(autocompleteDiv, {
@@ -57,7 +56,6 @@ function handleAddressSearch(event) {
 
 submitAddressSearch.addEventListener("click", handleAddressSearch);
 
-
 //Making only one selection to be selected
 
 function checkOnlyOne(element) {
@@ -65,21 +63,20 @@ function checkOnlyOne(element) {
 
   checkboxes.forEach((cb) => {
     cb.checked = false;
-  })
+  });
   element.checked = true;
 }
 
 //Adding validation to radius
-function validateform(){
-var EnterAddress = document.startYourjournal.Address.value;
-var EnterRadius = document.startYourjournal.Radius.value;
+function validateform() {
+  var EnterAddress = document.startYourjournal.Address.value;
+  var EnterRadius = document.startYourjournal.Radius.value;
 
-if (EnterAddress == null || EnterAddress=="") {
-  alert ("Address can't be blank");
-  return false;
-}
-else if(EnterRadius ==null || EnterRadius==""){
-  alert ("Please set radius!!");
-  return false;
-}
+  if (EnterAddress == null || EnterAddress == "") {
+    alert("Address can't be blank");
+    return false;
+  } else if (EnterRadius == null || EnterRadius == "") {
+    alert("Please set radius!!");
+    return false;
+  }
 }
